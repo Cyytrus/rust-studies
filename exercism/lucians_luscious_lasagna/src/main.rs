7805b3c6a86bb482732acaa1ddc922cd0d6cd98f
Cyytrus <antoniopaulo122@gmail.com>
2023-01-05 22:46:31 -0300

    feat: add exercism exercises

---
+++ exercism/lucians_luscious_lasagna/src/main.rs
@@ -0,0 +1,16 @@
+#![allow(unused)]
+fn main(){}
+
+pub fn expected_minutes_in_oven() -> i32 {
+    let minutes = 40;
+    minutes
+}
+pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
+    return expected_minutes_in_oven() - actual_minutes_in_oven;
+}
+pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
+    return number_of_layers * 2;
+}
+pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
+    return preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven;
+}
